469d6787e45df880792a5974b2c9f8eb6f841fd0
Elanchezhian2712 <elanchezhian2712.m@gmail.com>
2025-06-10 20:35:44 +0530

    lasto

--- src/app/services/ServicesPageClient.tsx
+++ src/app/services/ServicesPageClient.tsx
@@ -146,7 +146,7 @@ const ServicesPageClient = ({ cardData }: ServicesPageClientProps) => {
   const subText = "Our Services";
 
   return (
-    <div className='bg-black'>
+    <div>
       <div className={`h-[40rem] w-full rounded-md relative flex flex-col items-center justify-center antialiased border-b ${sectionBorderStyle}`}>
         <div className="max-w-5xl mx-auto p-4">
           <h1 className="relative z-10 text-lg md:text-7xl text-purple-500 text-center font-sans font-bold">
